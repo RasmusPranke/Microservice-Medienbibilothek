afc8ea01db651964e33e683c517a1ba6c118403b
Rattle22 <RasRas2@googlemail.com>
2019-01-20 18:56:10 +0100

    Moves dependencies to medien-library.

--- medienbestand-service/src/main/java/de/grzb/MedienbestandServiceApplication.java
+++ medienbestand-service/src/main/java/de/grzb/MedienbestandServiceApplication.java
@@ -1,7 +1,7 @@
 package de.grzb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;;
 
 @SpringBootApplication
 public class MedienbestandServiceApplication {

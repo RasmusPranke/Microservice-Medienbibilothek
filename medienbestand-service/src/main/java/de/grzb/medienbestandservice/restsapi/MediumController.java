65ab70fd55ca880943432a577d6154763bfeb01e
Nikolai Elich <6elich@informatik.uni-hamburg.de>
2019-01-26 15:39:18 +0100

    Restore specific import statement in MediumController

--- medienbestand-service/src/main/java/de/grzb/medienbestandservice/restsapi/MediumController.java
+++ medienbestand-service/src/main/java/de/grzb/medienbestandservice/restsapi/MediumController.java
@@ -7,7 +7,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import de.grzb.materialien.medien.CD;
 import de.grzb.materialien.medien.Medium;

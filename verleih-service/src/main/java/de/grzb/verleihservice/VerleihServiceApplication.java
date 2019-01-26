dc7b4763f71c4152e32992d5ac84828a1c5c56be
Nikolai Elich <6elich@informatik.uni-hamburg.de>
2019-01-26 16:17:59 +0100

    Add verleih-service

---
+++ verleih-service/src/main/java/de/grzb/verleihservice/VerleihServiceApplication.java
@@ -0,0 +1,14 @@
+package de.grzb.verleihservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerleihServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerleihServiceApplication.class, args);
+	}
+
+}
+
---
+++ verleih-service/src/test/java/de/grzb/verleihservice/VerleihServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package de.grzb.verleihservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VerleihServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

00e9e7ca395dceddfbd6240712e4f141fd83d006
Nikolai Elich <6elich@informatik.uni-hamburg.de>
2019-01-26 16:19:22 +0100

    Enable eureka discovery

--- verleih-service/src/main/java/de/grzb/verleihservice/VerleihServiceApplication.java
+++ verleih-service/src/main/java/de/grzb/verleihservice/VerleihServiceApplication.java
@@ -2,8 +2,10 @@ package de.grzb.verleihservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.*;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VerleihServiceApplication {
 
 	public static void main(String[] args) {
